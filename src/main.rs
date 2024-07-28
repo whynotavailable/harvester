ee3bb7e3b58e1574b6bad003a72a1d093573346c
Tom <JosephDotson@belkonar.com>
2024-07-27 20:31:40 -0400

    working

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-use axum::{
-    async_trait,
-    extract::{FromRequest, Request},
-    response::{IntoResponse, Response},
-    routing::post,
-    Json, RequestExt, Router,
-};
+use axum::{extract, routing::post, Json, Router};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -12,29 +6,10 @@ struct Payload {
     val: String,
 }
 
-async fn handler(JsonBody(payload): JsonBody<Payload>) -> Json<Payload> {
-    return Json(Payload {
+async fn handler(extract::Json(payload): extract::Json<Payload>) -> Json<Payload> {
+    Json(Payload {
         val: format!("Hi {}", payload.val),
-    });
-}
-
-struct JsonBody<T>(T);
-
-// impl IntoResponse<S> for JsonBody<T> {}
-
-#[async_trait]
-impl<S, T> FromRequest<S> for JsonBody<T>
-where
-    S: Send + Sync,
-    Json<T>: FromRequest<()>,
-    T: 'static,
-{
-    type Rejection = Response;
-
-    async fn from_request(req: Request, _state: &S) -> Result<Self, Self::Rejection> {
-        let Json(payload) = req.extract().await.map_err(IntoResponse::into_response)?;
-        return Ok(Self(payload));
-    }
+    })
 }
 
 #[tokio::main]
