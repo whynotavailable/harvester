73d692b6a3fa2df1798cf5b073f1769405f953a1
Tom <josephdotson@belkonar.com>
2025-05-18 09:06:59 -0400

    linter try 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,20 +2,21 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
-import stylisticJs from '@stylistic/eslint-plugin-js'
-
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
   { ignores: ['pkg/ui'] }, // It has it's own eslint
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js, stylisticJs }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js, stylistic }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-  stylisticJs.configs.all,
+  stylistic.configs.recommended,
   {
     rules: {
       "react/react-in-jsx-scope": "off",
       "@typescript-eslint/no-explicit-any": "off",
-      "@stylistic/js/multiline-comment-style": "off"
+      "@stylistic/js/multiline-comment-style": "off",
+      "@stylistic/semi": ["error", "always"],
+      "@stylistic/quotes": ["error", "single", { "avoidEscape": true }]
     }
   }
 ]);
